6daa20e35bf04654cd692aac35b417946051be31
dannyandreev <danny.andreev@gmail.com>
2021-07-29 11:32:45 -0700

    skeleton done

---
+++ client/pages/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Redirect from '../components/redirect';
+import TestButton from '../components/test-button';
+import AppContext from '../lib/app-context';
+
+
+export default class Home extends React.Component {
+  render() {
+
+    if (!this.context.user) return <Redirect to="sign-in" />;
+
+    return (
+      <div>
+        <TestButton></TestButton>
+      </div>
+    );
+  }
+}
+
+Home.contextType = AppContext;
