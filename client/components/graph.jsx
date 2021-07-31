8395b725cdf19c31fb617a214d0a42049d38a43a
dannyandreev <danny.andreev@gmail.com>
2021-07-30 19:28:52 -0700

    post request working

--- client/components/graph.jsx
+++ client/components/graph.jsx
@@ -5,124 +5,26 @@ import React from 'react'
 // import SyntaxHighlighter from 'components/SyntaxHighlighter'
 import { Chart } from 'react-charts'
 
-
-
-
 export default function makeGraph(){
-  const tempData = React.useMemo(
+
+  const series = React.useMemo(
+    () => ({
+      type: "line",
+      showPoints: false
+    }),
+    []
+  );
+
+  const axes = React.useMemo(
     () => [
-      {
-        label: "Series 1",
-        datums: [
-          {
-            x: new Date("2020-03-18T11:00:00.000Z"),
-            y: 60
-          },
-          {
-            x: new Date("2020-03-18T11:30:00.000Z"),
-            y: 23
-          },
-          {
-            x: new Date("2020-03-18T12:00:00.000Z"),
-            y: 65
-          },
-          {
-            x: new Date("2020-03-18T12:30:00.000Z"),
-            y: 84
-          },
-          {
-            x: new Date("2020-03-18T13:00:00.000Z"),
-            y: 87
-          },
-          {
-            x: new Date("2020-03-18T13:30:00.000Z"),
-            y: 84
-          },
-          {
-            x: new Date("2020-03-18T14:00:00.000Z"),
-            y: 96
-          },
-          {
-            x: new Date("2020-03-18T14:30:00.000Z"),
-            y: 88
-          },
-          {
-            x: new Date("2020-03-18T15:00:00.000Z"),
-            y: 63
-          },
-          {
-            x: new Date("2020-03-18T15:30:00.000Z"),
-            y: 60
-          }
-        ]
-      },
-      {
-        label: "Series 2",
-        datums: [
-          {
-            x: new Date("2020-03-18T11:00:00.000Z"),
-            y: 41
-          },
-          {
-            x: new Date("2020-03-18T11:30:00.000Z"),
-            y: 15
-          },
-          {
-            x: new Date("2020-03-18T12:00:00.000Z"),
-            y: 95
-          },
-          {
-            x: new Date("2020-03-18T12:30:00.000Z"),
-            y: 96
-          },
-          {
-            x: new Date("2020-03-18T13:00:00.000Z"),
-            y: 33
-          },
-          {
-            x: new Date("2020-03-18T13:30:00.000Z"),
-            y: 96
-          },
-          {
-            x: new Date("2020-03-18T14:00:00.000Z"),
-            y: 32
-          },
-          {
-            x: new Date("2020-03-18T14:30:00.000Z"),
-            y: 49
-          },
-          {
-            x: new Date("2020-03-18T15:00:00.000Z"),
-            y: 18
-          },
-          {
-            x: new Date("2020-03-18T15:30:00.000Z"),
-            y: 69
-          }
-        ]
-      }
+      { primary: true, position: "bottom", type: "time" },
+      { position: "left", type: "linear"}
     ],
     []
   );
 
-    const series = React.useMemo(
-      () => ({
-        type: "line",
-        showPoints: false
-      }),
-      []
-    );
-
-    const axes = React.useMemo(
-      () => [
-        { primary: true, position: "bottom", type: "time" },
-        { position: "left", type: "linear"}
-      ],
-      []
-    );
-
-    const UUIDtoName = {};
-    const dataSet = [];
+  const UUIDtoName = {};
+  const dataSet = [];
 
   const [data, setData] = React.useState(null);
 
@@ -157,20 +59,13 @@ export default function makeGraph(){
                 delete series.deviceName
                 delete series.Temperature
               }
-
             })
             .then(() => {
-
               setData(dataSet)
             });
         })
     }
   )
-
-  console.log(data)
-  console.log(tempData)
-  console.log(series)
-  console.log(axes)
 
   return (
     <div className="App bg-leaf p-3">
--- database/data.sql
+++ database/data.sql
@@ -16,6 +16,7 @@ values ('fa0c33ce-68ab-44ab-9ac8-16de4f47cd0a', 'Temperature', '25', '2021-07-30
   ('fa0c33ce-68ab-44ab-9ac8-16de4f47cd0a', 'Temperature', '24', '2021-07-30T09:54:48.684406Z'),
   ('fa0c33ce-68ab-44ab-9ac8-16de4f47cd0a', 'Temperature', '20', '2021-07-30T10:54:48.684406Z'),
   ('fa0c33ce-68ab-44ab-9ac8-16de4f47cd0a', 'Temperature', '15', '2021-07-30T11:54:48.684406Z'),
+  ('fa0c33ce-68ab-44ab-9ac8-16de4f47cd0a', 'Temperature', '10', '2021-07-30T12:54:48.684406Z'),
   ('66f45ce9-c93c-44f3-ae9b-614fbfb8f62e', 'Temperature', '23', '2021-07-30T02:54:48.684406Z'),
   ('66f45ce9-c93c-44f3-ae9b-614fbfb8f62e', 'Temperature', '24', '2021-07-30T03:54:48.684406Z'),
   ('66f45ce9-c93c-44f3-ae9b-614fbfb8f62e', 'Temperature', '25', '2021-07-30T04:54:48.684406Z'),
